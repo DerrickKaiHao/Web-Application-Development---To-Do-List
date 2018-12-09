d9483946878979a819ec5908c82b55fb6cbcc7ea
Derrick <derrick@Derricks-MacBook-Pro.local>
2018-12-09 15:37:52 +0800

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { AddCatComponent } from './add-cat/add-cat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    AddCatComponent
   ],
   imports: [
     BrowserModule
