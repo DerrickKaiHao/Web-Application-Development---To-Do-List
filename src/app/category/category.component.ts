d6bef9f6e09db71d1c90e17630bf4adbb095886c
Derrick <derrick@Derricks-MacBook-Pro.local>
2019-01-03 09:25:28 +0800

    Category Page Complete

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TaskService } from '../task.service';
 import { Category } from '../cat.model';
+import { Task } from '../task.model';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -11,6 +12,7 @@ export class CategoryComponent implements OnInit {
   constructor(public TaskService: TaskService) { }
 
   @Input() catList: Category[];
+  @Input() taskList: Task[];
   ngOnInit() {
     this.catList = this.TaskService.getCategory();
     this.TaskService.catAdded
@@ -18,6 +20,15 @@ export class CategoryComponent implements OnInit {
         this.catList = this.TaskService.getCategory();
 
       })
+
+      this.taskList = this.TaskService.getTask();
+    this.TaskService.taskAdded
+      .subscribe(() => {
+        this.taskList = this.TaskService.getTask();
+        
+        
+      })
+      
   }
   findTask(info){
     this.TaskService.pushTask(info);
